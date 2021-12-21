9266d81c0797825b36b92f740f8ad70c89b6013f
George Paterson <paterson.george@gmail.com>
2021-12-21 15:00:39 +0000

    Setting up better routes for supported and unsupported postcodes.

--- src/controllers/postcode.js
+++ src/controllers/postcode.js
@@ -1,32 +1,42 @@
 const express = require('express');
 const postcode = express.Router();
-const {postcodeValidator} = require('postcode-validator');
+const {postcodeValidator, postcodeValidatorExistsForCountry} = require('postcode-validator');
 
 /*
-*
+* Set up routes.
 */
 
 postcode.get('/postcode/:country/:postcode', function (req, res) {
   const country = req.params.country.toUpperCase();
   const code = req.params.postcode.toUpperCase();
-  const result = postcodeValidator(code, country);
-  res.send(result)
+  if(postcodeValidatorExistsForCountry(country)) {
+    if(postcodeValidator(code, country)) {
+      res.send({
+        status: 'Ok: Supported postcode for ' + country
+      });
+    } else {
+      res.send({
+        status: 'Error: Unsupported postcode for ' + country
+      });
+    }
+  } else {
+    res.send({
+      status: 'Error: Unsupported country'
+    });
+  }
 });
 
 postcode.get('/postcode/:postcode', function (req, res) {
   const code = req.params.postcode.toUpperCase();
-  const result = postcodeValidator(code, 'GB');
-  res.send(result)
+  if(postcodeValidator(code, 'GB')) {
+    res.send({
+      status: 'Ok: Supported postcode for GB'
+    });
+  } else {
+    res.send({
+      status: 'Error: Unsupported postcode for GB'
+    });
+  }
 });
-
-
-/*
-* TODO:
-* Check data source for country code.
-* If valid country code, check postcode against validator.
-* Default to UK postcode validator.
-* If valid postcode in service area, return true.
-*/
-
 
 module.exports = postcode;

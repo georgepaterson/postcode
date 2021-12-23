f1309bd72653867b4a28fb5e66fb3eccf5405b05
George Paterson <paterson.george@gmail.com>
2021-12-23 12:01:25 +0000

    Better structure to responses and more comments.

--- src/controllers/postcode.js
+++ src/controllers/postcode.js
@@ -8,47 +8,68 @@ const uk = require('../models/united-kingdom');
 * Set up routes.
 */
 
+/*
+* Check if a postcode is valid for the given country.
+*/
+
 postcode.get('/postcode/:country/:postcode', (req, res) => {
   const country = req.params.country.toUpperCase();
   const code = req.params.postcode.toUpperCase();
+  const response = country.toLowerCase();
+  // Check if the country is supported.
   if (postcodeValidatorExistsForCountry(country)) {
+    // Check if the postcode is valid for the country.
     if (postcodeValidator(code, country)) {
+      // UK test case for postcode using an if statement.
+      // When further testing is required, this can be replaced with a switch statement.
       if (uk.test(code)) {
-        res.send({
-          status: `Ok: Supported postcode for ${country}`,
+        res.send(200, {
+          valid: true,
+          description: `supported postcode for ${response} appointments`,
         });
       } else {
-        res.send({
-          status: `Error: Usupported postcode for ${country}`,
+        res.send(422, {
+          valid: false,
+          description: `unsupported postcode for ${response} appointments`,
         });
       }
     } else {
-      res.send({
-        status: `Error: Invalid postcode for ${country}`,
+      res.send(422, {
+        valid: false,
+        description: `invalid postcode for ${response} appointments`,
       });
     }
   } else {
-    res.send({
-      status: `Error: Invalid country code of ${country}`,
+    res.send(422, {
+      valid: false,
+      description: `invalid postcode for ${response} appointments`,
     });
   }
 });
+
+/*
+* Default route for postcodes with no country code.
+* Route will test against UK postcodes.
+*/
 
 postcode.get('/postcode/:postcode', (req, res) => {
   const code = req.params.postcode.toUpperCase();
   if (postcodeValidator(code, 'UK')) {
     if (uk.test(code)) {
-      res.send({
-        status: 'Ok: Supported postcode for UK',
+      res.send(200, {
+        valid: true,
+        description: `supported postcode for uk appointments`,
       });
     } else {
-      res.send({
-        status: 'Error: Usupported postcode for UK',
+      res.send(422, {
+        valid: false,
+        description: `unsupported postcode for uk appointments`,
       });
     }
   } else {
-    res.send({
-      status: 'Error: Invalid postcode for UK',
+    res.send(422, {
+      valid: false,
+      description: `invalid postcode for uk appointments`,
     });
   }
 });
